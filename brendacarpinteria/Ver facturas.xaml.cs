5ab918df33433e6699de0d72c944d258a21d60d6
KVN165 <99109800+KVN165@users.noreply.github.com>
2023-04-17 11:06:30 -0600

    Actualizacion ver facturas 17-04 1

--- brendacarpinteria/Ver facturas.xaml.cs
+++ brendacarpinteria/Ver facturas.xaml.cs
@@ -18,7 +18,8 @@ using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Windows.Forms;
-using MessageBox = System.Windows.Forms;
+//using MessageBox = System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 
 namespace brendacarpinteria
@@ -36,6 +37,10 @@ namespace brendacarpinteria
         //variables
         static int posicion;
 
+        string identidad_cliente;
+        string nombre_cliente;
+        string apellido_cliente;
+
         private string cadenaconexion = "Data Source = localhost\\sqlexpress; Initial Catalog = Carpinteria_BD; Integrated Security=True";
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -46,6 +51,7 @@ namespace brendacarpinteria
             dgvDetalles.ReadOnly = true;
             dgvDetalles.AllowUserToAddRows = false;
             dgvDetalles.AllowUserToDeleteRows = false;
+            
             Cargar_facturas();
         }
 
@@ -70,7 +76,10 @@ namespace brendacarpinteria
             finally
             {
                 conexion.Close();
+                dgvFactura.ClearSelection();
+                dgvDetalles.ClearSelection();
             }
+            posicion = -1;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -161,6 +170,148 @@ namespace brendacarpinteria
         }
 
         private void btnregresar2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            if (dgvFactura.CurrentRow == null)
+            {
+                MessageBox.Show("Seleccione una celda primero");
+            }
+            else
+            {
+                imprimirfact();
+            }
+
+        }
+
+        private void imprimirfact()
+        {
+            if (posicion == -1)
+            {
+                MessageBox.Show("Seleccione una factura para imprimir");
+            }
+            else
+            {
+                mostrar_factura();
+            }   
+        }
+
+        private void mostrar_factura()
+        {
+            brendacarpinteria.climprimirfactura.CreaTicket Ticket1 = new brendacarpinteria.climprimirfactura.CreaTicket();
+
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoCentro("Carpinteria Brenda"); //imprime una linea de descripcion
+            Ticket1.TextoCentro("**********************************");
+
+            Ticket1.TextoCentro("Factura de Venta"); //imprime una linea de descripcion
+            Ticket1.TextoIzquierda("No Factura: " + dgvFactura[0, posicion].Value.ToString());
+
+            //convertir fecha hora
+            string fechahorastring = dgvFactura[3, posicion].Value.ToString();
+
+            DateTime fechahora = DateTime.Parse(fechahorastring);
+
+            DateTime fecha = fechahora.Date;
+            TimeSpan hora = fechahora.TimeOfDay;
+            // fin
+
+            Ticket1.TextoIzquierda("Fecha: " + fecha.ToString("dd/mm/yyyy") + "        Hora:" + hora.ToString());
+            Ticket1.TextoIzquierda("Le Atendió: " + dgvFactura[1, posicion].Value.ToString());
+
+
+            //Buscando cual es la id del cliente
+            SqlConnection conexion = new SqlConnection(@"Data Source=localhost\sqlexpress; Initial Catalog=Carpinteria_BD; Integrated Security=True;");
+                conexion.Open();
+                string nombrecompleto = dgvFactura[2, posicion].Value.ToString();
+                string[] dividir_nombre = nombrecompleto.Split(' ');
+                SqlCommand cm = new SqlCommand("SELECT * FROM clientes WHERE nombre='" + dividir_nombre[0] + "' ;", conexion);
+                SqlDataReader dr = cm.ExecuteReader();
+                while (dr.Read())
+                {
+                    identidad_cliente = dr["id_cliente"].ToString();
+                    nombre_cliente = dr["nombre"].ToString();
+                    apellido_cliente = dr["apellido"].ToString();
+                }
+                conexion.Close();
+           
+
+            //POINT
+            Ticket1.TextoIzquierda("Identidad Cliente: "+ identidad_cliente.ToString());
+            Ticket1.TextoIzquierda("Cliente: "+ nombre_cliente.ToString()+ " " + apellido_cliente.ToString());
+            Ticket1.TextoIzquierda("");
+            brendacarpinteria.climprimirfactura.CreaTicket.LineasGuion();
+
+            brendacarpinteria.climprimirfactura.CreaTicket.EncabezadoVenta();
+            brendacarpinteria.climprimirfactura.CreaTicket.LineasGuion();
+
+            
+            foreach (System.Windows.Forms.DataGridViewRow r in dgvDetalles.Rows)
+            {
+                // Articulo                     //Precio                                    cantidad                            Subtotal
+                Ticket1.AgregaArticulo(r.Cells[2].Value.ToString(), double.Parse(r.Cells[4].Value.ToString()), int.Parse(r.Cells[3].Value.ToString()), double.Parse(r.Cells[5].Value.ToString())); //imprime una linea de descripcion
+            }
+
+
+            brendacarpinteria.climprimirfactura.CreaTicket.LineasGuion();
+            Ticket1.AgregaTotales("Sub-Total", double.Parse(dgvFactura[5, posicion].Value.ToString())); // imprime linea con Subtotal
+            Ticket1.AgregaTotales("15%. ISV", double.Parse(dgvFactura[6, posicion].Value.ToString()));
+            Ticket1.AgregaTotales("Total", double.Parse(dgvFactura[7, posicion].Value.ToString())); // imprime linea con total
+            Ticket1.AgregaTotales("Entrega:", double.Parse(dgvFactura[4, posicion].Value.ToString()));
+            //calculo cambio
+            decimal cambio;
+            cambio = Convert.ToDecimal(dgvFactura[4, posicion].Value.ToString()) - Convert.ToDecimal(dgvFactura[7, posicion].Value.ToString());
+            //fin
+            Ticket1.AgregaTotales("Cambio:", double.Parse(Convert.ToString(cambio)));
+           
+
+            // Ticket1.LineasTotales(); // imprime linea 
+
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoCentro("*     Gracias por preferirnos    *");
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoIzquierda(" ");
+            string impresora = "Microsoft XPS Document Writer";
+            Ticket1.ImprimirTiket(impresora);
+
+
+
+            /*
+            Fila = 0;
+            while (dataGridView1.RowCount > 0)//limpia el dgv
+            { dataGridView1.Rows.Remove(dataGridView1.CurrentRow); }
+            //LBLIDnuevaFACTURA.Text = ClaseFunciones.ClsFunciones.IDNUEVAFACTURA().ToString();
+
+            txtIdProducto.Text = lblNombre.Text =  txtCantidad.Text =textBox3.Text= "";
+            lblCostoApagar.Text = lbldevolucion.Text = lblPrecio.Text = "0";
+            txtIdProducto.Focus();
+            MessageBox.Show("Gracias por preferirnos");
+            */
+            
+        }
+
+        private void limpiar(object sender, RoutedEventArgs e)
+        {
+            posicion = -1;
+            MessageBox.Show("posicion: "+posicion);
+            dgvFactura.ClearSelection();
+
+            if(dgvFactura.CurrentRow == null)
+            {
+                MessageBox.Show("Es nulo");
+            }
+            else
+            {
+                MessageBox.Show("No es nulo");
+            }
+        }
+
+        private void btncerrar_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
